6e05986f7a93df6ffe3db0c2756e95326c5c450e
FunnyPocketBook <ying-kai.dang@outlook.com>
2022-11-11 01:15:01 +0100

    start getting categories

--- backend/src/classes/Product.py
+++ backend/src/classes/Product.py
@@ -1,11 +1,48 @@
+from supermarktconnector.ah import AHConnector
+import pandas as pd
+import re
+
+
 class Product:
-    def __init__(self, quantity: float=None, unit: str=None, name: str=None, price: float=None, total_price: float=None, indicator: str=None):
+    _connector = AHConnector()
+
+    def __init__(self, quantity: float=None, unit: str=None, description: str=None, price: float=None, total_price: float=None, indicator: str=None):
         self.quantity = quantity
         self.unit = unit
-        self.name = name
+        self.description = description
+        self.name = self._get_name()
         self.price = price
         self.total_price = total_price
         self.indicator = indicator
 
+    
+    def _set_details(self) -> str:
+        """Fetch and set the details of the product."""
+        result = self.connector.search_products(query=self.description, size=20, page=0)
+        df = pd.DataFrame(result["products"], columns=["title", "unitPriceDescription", "priceBeforeBonus", "mainCategory", "subCategory", "subCategoryId", "brand", "shopType"])
+        df["unitPriceDescription"] = df["unitPriceDescription"].apply(self._clean_unit_price_description)
+        if self.unit.lower() == "kg":
+            row = df[df["unitPriceDescription"] == self.price]
+        elif self.quantity == 1:
+            row = df[df["priceBeforeBonus"] == self.total_price]
+        else:
+            row = df[df["priceBeforeBonus"] == self.price]
+    
+
+    def _get_brand(self) -> str:
+        """Get the brand of the product."""
+        return self.connector.search_products(query=self.description, size=20, page=0)["products"][0]["brand"]
+
+
+    def _clean_unit_price_description(self, unit_price_description: str) -> float:
+        """Clean the unit price description."""
+        regex = r"(\d+.\d+)"
+        return float(re.find(regex, unit_price_description)[0])
+
+    
+    @property
+    def connector(self):
+        return type(self)._connector
+
     def __repr__(self):
-        return f"Product(quantity={self.quantity}, unit={self.unit}, description={self.name}, price={self.price}, total_price={self.total_price}, indicator={self.indicator})"
+        return f"Product(quantity={self.quantity}, unit={self.unit}, name={self.name}, price={self.price}, total_price={self.total_price}, indicator={self.indicator})"
