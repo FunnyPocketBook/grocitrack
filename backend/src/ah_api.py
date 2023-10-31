45dfe3fd0421a092ca5c09f8d48fd5aaa23ca45a
FunnyPocketBook <ying-kai.dang@outlook.com>
2023-10-31 08:34:26 +0100

    Add previously bought items

--- backend/src/ah_api.py
+++ backend/src/ah_api.py
@@ -1,4 +1,5 @@
 import requests
+import urllib.parse
 from config import Config
 
 config = Config()
@@ -7,6 +8,7 @@ LOGIN_URL = "https://login.ah.nl/secure/oauth/authorize?client_id=appie&redirect
 TOKEN_URL = "https://api.ah.nl/mobile-auth/v1/auth/token"
 REFRESH_TOKEN_URL = "https://api.ah.nl/mobile-auth/v1/auth/token/refresh"
 RECEIPTS_URL = "https://api.ah.nl/mobile-services/v1/receipts"
+PREV_BOUGHT_URL = "https://api.ah.nl/mobile-services/product/search/v2/purchases?filters=previouslyBought%3Dpreviously_bought&sortOn=PURCHASE_DEPARTMENT&size=30&page=0"
 
 def login():
     """Uses the code from the config file to fetch the access token and refresh token.
@@ -74,3 +76,42 @@ def fetch_receipts():
         response = requests.get(RECEIPTS_URL, headers={"Authorization": f"Bearer {config.get('api')['access_token']}"})
     response.raise_for_status()
     return response.json()
+
+def get_previously_bought(sort_on: str="PURCHASE_DATE", size: int=100, page: int=0, result: list=None):
+    if result is None:
+        result = []
+    response = requests.get(
+        "https://api.ah.nl/mobile-services/product/search/v2/purchases",
+        headers={
+            "Authorization": f"Bearer {config.get('api')['access_token']}",
+            "X-Application": "AHWEBSHOP"
+        },
+        params={
+            "filters": "previouslyBought%3Dpreviously_bought",
+            "sortOn": sort_on,
+            "size": size,
+            "page": page
+        }
+    )
+    if response.status_code == 401:
+        update_tokens()
+        response = requests.get(PREV_BOUGHT_URL,
+            headers={
+                "Authorization": f"Bearer {config.get('api')['access_token']}",
+                "X-Application": "AHWEBSHOP"
+            },
+        )
+    response.raise_for_status()
+
+    
+    prev_bought = response.json()
+    result.extend(prev_bought["products"])
+    if "next" in prev_bought["links"]:
+        url = prev_bought["links"]["next"]["href"]
+        # get the page number and size from the url
+        parsed = urllib.parse.urlparse(url)
+        params = urllib.parse.parse_qs(parsed.query)
+        page = int(params["page"][0])
+        size = int(params["size"][0])
+        get_previously_bought(page=page, size=size, result=result)
+    return result
