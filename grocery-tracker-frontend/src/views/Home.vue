605207708e2912065d431e4340ff0b0eeb9a1f3f
FunnyPocketBook <ying-kai.dang@outlook.com>
2023-12-25 14:05:19 +0100

    frontend stuff

--- grocery-tracker-frontend/src/views/Home.vue
+++ grocery-tracker-frontend/src/views/Home.vue
@@ -1,17 +1,45 @@
 <template>
 	<HelloWorld />
-	<v-btn @click="onSubmit">Submit</v-btn>
-	<p>{{ JSON.stringify(data) }}</p>
+	<v-btn @click="getReceipts">Submit</v-btn>
+	<v-data-table
+		v-if="receipts.length > 0"
+		:headers="headers"
+		:items="receipts"
+		item-value="name"
+		class="elevation-1"
+	></v-data-table>
 </template>
 
 <script lang="ts" setup>
-	import { ref } from "vue";
+	import { ref, onMounted } from "vue";
 	import HelloWorld from "@/components/HelloWorld.vue";
 
-	const data = ref({});
-	async function onSubmit() {
+	const receipts = ref([]);
+	const headers = ref([
+		{
+			title: "Date",
+			align: "start",
+			key: "datetime",
+		},
+		{
+			title: "Total amount",
+			align: "start",
+			key: "total_price",
+		},
+		{
+			title: "Total discount",
+			align: "start",
+			key: "total_discount",
+		},
+	]);
+
+	async function getReceipts() {
 		const response = await fetch("http://localhost:5000/api/receipts");
 		const json = await response.json();
-		data.value = json;
+		receipts.value = json;
 	}
-</script>
+
+	onMounted(async () => {
+		getReceipts();
+	});
+</script> 
---
+++ grocery-tracker-frontend/src/views/Receipts.vue
@@ -0,0 +1,15 @@
+<template>
+	<v-btn @click="onSubmit">Submit</v-btn>
+	<p>{{ JSON.stringify(data) }}</p>
+</template>
+
+<script lang="ts" setup>
+	import { ref } from "vue";
+
+	const data = ref({});
+	async function onSubmit() {
+		const response = await fetch("http://localhost:5000/api/receipts");
+		const json = await response.json();
+		data.value = json;
+	}
+</script> 
