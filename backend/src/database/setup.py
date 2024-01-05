2d2dcae07290e86ec0b745eb01ec894b2edb850f
FunnyPocketBook <ying-kai.dang@outlook.com>
2024-01-06 00:35:47 +0100

    Update to psycopg v3

--- backend/src/database/setup.py
+++ backend/src/database/setup.py
@@ -1,10 +1,25 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from config import Config
 from database.model import Base
 
 
 config = Config()
 engine = create_engine(
-        f"postgresql://{config.get('database')['username']}:{config.get('database')['password']}@{config.get('database')['host']}/{config.get('database')['name']}"
-    )
+    f"postgresql+psycopg://{config.get('database')['username']}:{config.get('database')['password']}@{config.get('database')['host']}/{config.get('database')['database_name']}",
+    pool_size=20,
+    max_overflow=10,
+)
 Base.metadata.create_all(engine)
+
+sql_setup_statements = [
+    "CREATE EXTENSION IF NOT EXISTS pg_trgm;",
+    "CREATE INDEX IF NOT EXISTS previous_products_sub_category_gin_index ON previous_products USING gin(sub_category gin_trgm_ops);",
+    "CREATE INDEX IF NOT EXISTS previous_products_title_gin_index ON previous_products USING gin(title gin_trgm_ops);",
+    "CREATE INDEX IF NOT EXISTS ah_products_sub_category_gin_index ON ah_products USING gin(sub_category gin_trgm_ops);",
+    "CREATE INDEX IF NOT EXISTS ah_products_title_gin_index ON ah_products USING gin(title gin_trgm_ops);",
+]
+
+with engine.connect() as connection:
+    for statement in sql_setup_statements:
+        connection.execute(text(statement))
+    connection.commit()
